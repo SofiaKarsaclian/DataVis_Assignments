fad450c97bfe6800af054ebc0dfca66be52d9d93
SofiaKarsaclian <116818992+SofiaKarsaclian@users.noreply.github.com>
2024-04-15 14:41:55 +0200

    exercise 1

---
+++ Exercise01/index.js
@@ -0,0 +1,11 @@
+//TASK 4a
+//let the console tell that the script is loaded
+
+// TASK 4b
+// Select the five circles in the svg container.
+
+// Retrieve the x- and y-values and calculate the mean position.
+
+// Add a square to the svg container at the mean coordinates.
+
+// Print the mean values to the console.
