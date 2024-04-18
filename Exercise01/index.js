4d5905ee63ff1a9141973c9d57a0bfd3591de505
jian06feng <jian.feng@uni-konstanz.de>
2024-04-18 23:52:37 +0200

    task 1-2
    finished task 1-2, task 4 seems can be improved

--- Exercise01/index.js
+++ Exercise01/index.js
@@ -1,11 +1,21 @@
 //TASK 4a
 //let the console tell that the script is loaded
-
+console.log('The JavaScript file has been loaded.');
 // TASK 4b
 // Select the five circles in the svg container.
+const circles = document.querySelectorAll('svg circle');
+console.log(circles)
+
+
+
 
 // Retrieve the x- and y-values and calculate the mean position.
 
 // Add a square to the svg container at the mean coordinates.
 
 // Print the mean values to the console.
+
+
+
+
+// Select all five circles in the SVG container
